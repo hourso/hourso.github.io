652a8950eda48f2633305ecc8e9cac27eed8a142
hourso <hourso@users.noreply.github.com>
2025-12-12 14:09:01 +0000

    deploy: hourso/blog-source@2b93a95e6a928ff674a9b72ff9743a8452e23138

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Comprendre Kerberos simplement",
+        "excerpt":"Contenu de l’article…  ","categories": ["ActiveDirectory"],
+        "tags": ["kerberos","authentification"],
+        "url": "/activedirectory/comprendre-kerberos-simplement/",
+        "teaser": null
+      }]
