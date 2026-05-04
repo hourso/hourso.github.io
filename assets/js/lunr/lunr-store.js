7fef1d6217a23bc02272c69ad38d42d2042ff93a
hourso <hourso@users.noreply.github.com>
2026-05-04 19:11:31 +0000

    deploy: hourso/blog-source@d5d5a7fb7d8fb25e811a441994f83bfc6bec1f28

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
-        "title": "Comprendre Kerberos simplement",
-        "excerpt":"Introduction : pourquoi parler de Kerberos ? Si tu travailles dans un environnement Windows, que ce soit en entreprise ou dans un SOC, tu as forcément entendu parler de Kerberos. C’est le protocole d’authentification par défaut d’Active Directory depuis Windows 2000. Et pourtant, peu de gens savent vraiment comment il...","categories": ["active directory"],
-        "tags": ["kerberos","active directory","authentification","cybersecurité","SOC","detection"],
-        "url": "/active%20directory/comprendre-kerberos-simplement/",
-        "teaser": "/assets/images/kerberos/kerberos-flow.svg"
+        "title": "Introduction : pourquoi parler de Kerberos ?",
+        "excerpt":"Si tu travailles dans un environnement Windows, que ce soit en entreprise ou dans un SOC, tu as forcément entendu parler de Kerberos. C’est le protocole d’authentification par défaut d’Active Directory depuis Windows 2000. Et pourtant, peu de gens savent vraiment comment il fonctionne. Pourquoi c’est important pour toi, en...","categories": ["AD"],
+        "tags": [],
+        "url": "/ad/comprendre-kerberos-simplement/",
+        "teaser": "/assets/images/kerberos-header-teaser.jpg"
       }]
