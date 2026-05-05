8ec134d559efb0ec6c76e65e38ebd88915821947
hourso <hourso@users.noreply.github.com>
2026-05-05 17:45:12 +0000

    deploy: hourso/blog-source@52fd63d4c6262ab1f1284611596be69920b96f9e

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -3,5 +3,5 @@ var store = [{
         "excerpt":"Introduction : pourquoi parler de Kerberos ? Si tu travailles dans un environnement Windows, que ce soit en entreprise ou dans un SOC, tu as forcément entendu parler de Kerberos. C’est le protocole d’authentification par défaut d’Active Directory depuis Windows 2000. Et pourtant, peu de gens savent vraiment comment il...","categories": ["active directory"],
         "tags": ["kerberos","active directory","authentification","cybersecurité","SOC","detection"],
         "url": "/active%20directory/comprendre-kerberos-simplement/",
-        "teaser": "/assets/images/kerberos/kerberos-flow.svg"
+        "teaser": "/assets/images/Kerberos/kerberos-flow.svg"
       }]
